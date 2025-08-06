58260133b61e444155581e58d7fb01a686951825
Chris Searle <chris@chrissearle.org>
2025-08-06 09:16:10 +0200

    Update to nuxt 4

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,11 +6,8 @@ if ('CUPCAKE_FRONTEND' in process.env && process.env.CUPCAKE_FRONTEND !== undefi
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: '2025-08-06',
 
-  future: {
-    compatibilityVersion: 4,
-  },
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/icon', 'vuetify-nuxt-module'],
   vite: {
